0bfd1e23238914ee8ffc708713e45d3a926b97a3
Utkarsh Nawalgaria <utkarsh.n@olivecloud.in>
2022-09-19 23:38:38 +0530

    Fix: Added React Router to the project
    In this commit, React Router has been added to provide navigation to the website.
    I have also setup a basic `ProtectedRoute` component to use when a route needs authentication from the user.
    Also, added tailwind Forms plugin

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import { AuthProvider } from "./context/AuthProvider";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
-)
+);
