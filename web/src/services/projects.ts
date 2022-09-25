55528c54a8619fc58651bdae634dad221f31e3f8
Utkarsh Nawalgaria <utkarsh.n@olivecloud.in>
2022-09-26 00:01:05 +0530

    fix: created a new project service object to allow user to fetch all their projects, delete the projects, or create new projects

---
+++ web/src/services/projects.ts
@@ -0,0 +1,35 @@
+import client from "./client";
+
+export type Project = {
+  id: number;
+  title: string;
+  description?: string;
+  created_at: string;
+  updated_at?: string;
+  owner_id?: number;
+};
+
+export type ProjectCreate = {
+  title: string;
+  description?: string;
+};
+
+export default {
+  getAll: () => {
+    return client<Project[]>("project");
+  },
+  getById: (projectId: number) => {
+    return client<Project>(`project/${projectId}`);
+  },
+  createProject: (project: ProjectCreate) => {
+    return client<Project>("project", {
+      method: "POST",
+      body: JSON.stringify(project),
+    });
+  },
+  delete: (projectId: number) => {
+    return client(`project/${projectId}`, {
+      method: "delete",
+    });
+  },
+};
