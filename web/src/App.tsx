f006ecbc95995be83813e74bc850ded5bce90c45
Utkarsh Nawalgaria <utkarsh.n@olivecloud.in>
2022-09-26 00:04:23 +0530

    fix: Add new routes to the application

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home";
-import BooksPage from "./pages/books";
-import ProtectedRoute from "./protectedRoute";
+import ProtectedRoute from "./components/protectedRoute";
+import ProjectsPage from "./pages/projects";
+import SingleProjectPage from "./pages/single-project";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route element={<ProtectedRoute />}>
-        <Route path="/books" element={<BooksPage />}></Route>
+      <Route path="dashboard" element={<ProtectedRoute />}>
+        <Route path="projects" element={<ProjectsPage />} />
+        <Route path="projects/:projectId" element={<SingleProjectPage />} />
       </Route>
+      <Route path="*" element={<div>404</div>} />
     </Routes>
   );
 }
