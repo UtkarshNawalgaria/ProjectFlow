ff70a84b97891b66be1ab199ea220f17de3e967b
Utkarsh Nawalgaria <utkarsh.n@olivecloud.in>
2022-09-26 00:05:17 +0530

    fix: modified HomePage component to only show login form when not athenticated, otherwise user will be redirected to their dashboard page

--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -1,15 +1,14 @@
 import { FormEvent, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import useAuth from "../context/AuthProvider";
 
 const HomePage = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-  const { login, auth } = useAuth();
+  const { login, auth, redirectUrl } = useAuth();
 
   useEffect(() => {
     if (auth?.accessToken) {
-      window.location.assign("/books");
+      window.location.assign(redirectUrl);
     }
   }, []);
 
@@ -20,44 +19,37 @@ const HomePage = () => {
 
   return (
     <div className="h-screen flex items-center justify-center">
-      {auth?.accessToken ? (
-        <div>
-          <p>User Logged in</p>
-          <Link to={"/books"}>Books</Link>
-        </div>
-      ) : (
-        <form onSubmit={(e) => handleLoginFormSubmit(e)}>
-          <div className="flex flex-col gap-[5px]">
-            <label>Email:</label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="mb-8 rounded-md"
-              placeholder="Enter your email"
-            />
-          </div>
-          <div className="flex flex-col gap-[5px]">
-            <label>Password:</label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="mb-8 rounded-md"
-              placeholder="Enter password..."
-            />
-          </div>
+      <form onSubmit={(e) => handleLoginFormSubmit(e)}>
+        <div className="flex flex-col gap-[5px]">
+          <label>Email:</label>
           <input
-            className="cursor-pointer py-2 rounded-md font-bold w-full bg-red-300"
-            type="submit"
-            value="Login"
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="mb-8 rounded-md"
+            placeholder="Enter your email"
           />
-        </form>
-      )}
+        </div>
+        <div className="flex flex-col gap-[5px]">
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="mb-8 rounded-md"
+            placeholder="Enter password..."
+          />
+        </div>
+        <input
+          className="cursor-pointer py-2 rounded-md font-bold w-full bg-red-300"
+          type="submit"
+          value="Login"
+        />
+      </form>
     </div>
   );
 };
