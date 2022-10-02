f619f45fe76b7e65e14a805798ed3796c6ff6605
Utkarsh Nawalgaria <utkarsh.n@olivecloud.in>
2022-10-02 16:52:20 +0530

    fix: Configure Toast Notifications in App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,27 @@
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/home";
+import { ToastContainer } from "react-toastify";
+
 import ProtectedRoute from "./components/protectedRoute";
-import ProjectsPage from "./pages/projects";
+
+import HomePage from "./pages/home";
+import ProjectsPage from "./pages/project-list";
 import SingleProjectPage from "./pages/single-project";
+
+import "react-toastify/dist/ReactToastify.min.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="" element={<ProtectedRoute />}>
-        <Route path="projects" element={<ProjectsPage />} />
-        <Route path="projects/:projectId" element={<SingleProjectPage />} />
-      </Route>
-      <Route path="*" element={<div>404</div>} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="" element={<ProtectedRoute />}>
+          <Route path="projects" element={<ProjectsPage />} />
+          <Route path="projects/:projectId" element={<SingleProjectPage />} />
+        </Route>
+        <Route path="*" element={<div>404</div>} />
+      </Routes>
+      <ToastContainer />
+    </>
   );
 }
 
