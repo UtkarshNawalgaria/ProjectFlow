701a7e6a0c69ce3ad84d256688023e18ed244664
Utkarsh Nawalgaria <utkarsh.n@olivecloud.in>
2022-10-06 18:36:57 +0530

    fix: update `Button` component to accept icons as well

--- web/src/components/button.tsx
+++ web/src/components/button.tsx
@@ -1,21 +1,27 @@
 type ButtonProps = {
   text: string;
   type: "CANCEL" | "CONFIRM" | "DANGER";
-  onClick?: () => void;
+  icon: JSX.Element;
+  onClick: () => void;
 };
 
-const Button = ({ text, type, onClick }: ButtonProps) => {
+const Button = ({ text, type, icon, onClick }: ButtonProps) => {
   const typetoColor = {
     CONFIRM: "bg-primary text-white",
     CANCEL: "text-primary",
     DANGER: "bg-error text-white",
   };
 
+  const btnStyleWithIcon = "flex items-center gap-2";
+
   return (
     <button
       onClick={onClick}
-      className={`rounded-md font-semibold cursor-pointer py-2 px-6 outline outline-1 ${typetoColor[type]}`}>
-      {text}
+      className={`rounded-md font-semibold cursor-pointer py-2 px-6 outline outline-1 hover:bg-indigo-600 transition ${
+        typetoColor[type]
+      } ${icon ? btnStyleWithIcon : null}`}>
+      <span>{text}</span>
+      {icon ? <span>{icon}</span> : null}
     </button>
   );
 };
