01daa859c00655012e5994be88b79b34bbaa880b
Utkarsh Nawalgaria <utkarsh.n@olivecloud.in>
2022-10-20 18:40:14 +0530

    change padding of user initials logo in header

--- web/src/components/layouts/authenticated.tsx
+++ web/src/components/layouts/authenticated.tsx
@@ -21,7 +21,7 @@ const AuthenticatedLayout = ({ children }: { children: JSX.Element }) => {
       <header className="logo p-3 shadow-md flex justify-between items-center">
         <div className="font-extrabold text-3xl">Tasks</div>
         <div className="mr-2 flex gap-2 items-center hover:bg-gray-100 px-2 py-1 cursor-pointer rounded-md">
-          <span className="bg-red-100 py-1 px-2 rounded-full">{initials}</span>
+          <span className="bg-red-100 p-2 rounded-full">{initials}</span>
           <span>{user?.user.name}</span>
         </div>
       </header>
