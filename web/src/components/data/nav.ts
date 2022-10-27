a78dd65043f50017da1ad1c45f37e6e330beae64
Utkarsh Nawalgaria <utkarsh.n@olivecloud.in>
2022-10-27 23:56:38 +0530

    Closes #9
    In the `Sidebar` component, added a new dropdown,
    showing all of the user's organization.

---
+++ web/src/components/data/nav.ts
@@ -0,0 +1,38 @@
+export const organizationNavItems = [
+  {
+    id: 1,
+    path: "/settings",
+    title: "Organization Settings",
+    icon: "settings",
+  },
+  {
+    id: 2,
+    path: "/members",
+    title: "Team Members",
+    icon: "members",
+  },
+];
+
+export const profileNavItems = [
+  {
+    id: 1,
+    path: "/profile",
+    title: "Profile Settings",
+    icon: "profile",
+  },
+];
+
+export default [
+  {
+    id: 1,
+    path: "/dashboard",
+    title: "Dashboard",
+    icon: "dashboard",
+  },
+  {
+    id: 2,
+    path: "/projects",
+    title: "Projects",
+    icon: "projects",
+  },
+];
--- web/src/components/sidebar.tsx
+++ web/src/components/sidebar.tsx
@@ -1,8 +1,24 @@
+import {
+  AiOutlineFundProjectionScreen,
+  AiOutlineHome,
+  AiOutlineSetting,
+  AiOutlineUsergroupAdd,
+} from "react-icons/ai";
 import { Link, useLocation } from "react-router-dom";
-import navItems from "./data/nav.json";
+import useUser, { TUserContext } from "../context/UserProvider";
+import navItems, { organizationNavItems } from "./data/nav";
+import Organizations from "./organizations";
+
+const IconMap: { [key: string]: JSX.Element } = {
+  dashboard: <AiOutlineHome className="h-5 w-5" />,
+  projects: <AiOutlineFundProjectionScreen className="h-5 w-5" />,
+  settings: <AiOutlineSetting className="h-5 w-5" />,
+  members: <AiOutlineUsergroupAdd className="h-5 w-5" />,
+};
 
 const Sidebar = () => {
   const location = useLocation();
+  const { currentOrganization } = useUser() as TUserContext;
 
   const isPathActive = (linkPath: string, isLink = true): string => {
     const returnCss = isLink
@@ -13,26 +29,56 @@ const Sidebar = () => {
   };
 
   return (
-    <nav className="mt-4">
-      <ul className="">
-        {navItems.map((item, index) => (
-          <li
-            key={index}
-            className={`py-2 pl-4 cursor-pointer hover:bg-grey-lightest hover:border-r-8 hover:border-primary mb-2 ${isPathActive(
-              item.path
-            )}`}>
-            <Link
-              className={`block text-grey-dark font-bold hover:text-primary active:text-primary ${isPathActive(
-                item.path,
-                false
-              )}`}
-              to={item.path}>
-              {item.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <div>
+      <Organizations />
+      <nav className="mt-4">
+        <div>
+          <ul className="border-b pb-2">
+            {navItems.map((item, index) => (
+              <li
+                key={index}
+                className={`py-2 pl-4 cursor-pointer hover:bg-grey-lightest hover:border-r-8 hover:border-primary mb-2 ${isPathActive(
+                  item.path
+                )}`}>
+                <Link
+                  className={`flex items-center gap-4 text-grey-dark font-bold hover:text-primary active:text-primary ${isPathActive(
+                    item.path,
+                    false
+                  )}`}
+                  to={item.path}>
+                  {item.icon ? <span>{IconMap[item.icon]}</span> : null}
+                  <span>{item.title}</span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <span className="block pl-4 mt-4 text-lg">
+            {currentOrganization?.title}
+          </span>
+        </div>
+        <ul className="border-b pb-2 mt-4">
+          {organizationNavItems.map((item, index) => (
+            <li
+              key={index}
+              className={`py-2 pl-4 cursor-pointer hover:bg-grey-lightest hover:border-r-8 hover:border-primary mb-2 ${isPathActive(
+                item.path
+              )}`}>
+              <Link
+                className={`flex items-center gap-4 text-grey-dark font-bold hover:text-primary active:text-primary ${isPathActive(
+                  item.path,
+                  false
+                )}`}
+                to={`/organization/${currentOrganization?.id}${item.path}`}>
+                {item.icon ? <span>{IconMap[item.icon]}</span> : null}
+                <span>{item.title}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
   );
 };
 
