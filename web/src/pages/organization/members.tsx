7a88b661f8c4e17ad2e5c1505c60da67b90df1a4
Utkarsh Nawalgaria <utkarsh.n@olivecloud.in>
2022-10-27 23:59:40 +0530

    New pages added
    Added two new pages,
    1. Organization Settings
    2. Organization Members

---
+++ web/src/pages/organization/members.tsx
@@ -0,0 +1,12 @@
+const OrganizationMembers = () => {
+  return (
+    <div className="px-4">
+      <section className="mb-8">
+        <h1 className="text-2xl text-grey-dark font-bold">Members</h1>
+      </section>
+      <section>Members list</section>
+    </div>
+  );
+};
+
+export default OrganizationMembers;
