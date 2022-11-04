3bafdd2c7ef6c1c929a9e14a5f20f6743f6571b8
Utkarsh Nawalgaria <utkarsh.n@olivecloud.in>
2022-11-04 18:09:19 +0530

    fix: Adding AnonymousLayout as parent component for all routes that are unauthenticated and minor UI changes

--- web/src/pages/verify-email.tsx
+++ web/src/pages/verify-email.tsx
@@ -2,7 +2,6 @@ import { FormEvent, useRef, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import Button from "../components/button";
-import AnonymousLayout from "../components/layouts/anonymous";
 import client from "../services/client";
 
 const VerifyEmail = () => {
@@ -32,36 +31,34 @@ const VerifyEmail = () => {
   };
 
   return (
-    <AnonymousLayout>
-      <div className="h-full w-full flex items-center justify-center">
-        {isVerified ? (
-          <div className="relative">
-            <img src="./images/email-verified.png" alt="Email Verified" />
-            <p className="absolute text-4xl font-semibold text-[#413f56] top-[60px] left-1/2 -translate-x-1/2 ">
-              You email has been verified
-            </p>
-          </div>
-        ) : (
-          <form onSubmit={handleFormSubmit}>
-            <div className="mb-4">
-              <input
-                ref={emailRef}
-                type="email"
-                required
-                className="rounded-md border focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary w-full"
-                placeholder="Email"
-              />
-            </div>
-            <Button
-              as="button"
-              text="Verify Email"
-              type="CONFIRM"
-              extraStyles={"w-full"}
+    <div className="h-full w-full flex items-center justify-center">
+      {isVerified ? (
+        <div className="relative">
+          <img src="./images/email-verified.png" alt="Email Verified" />
+          <p className="absolute text-4xl font-semibold text-[#413f56] top-[60px] left-1/2 -translate-x-1/2 ">
+            You email has been verified
+          </p>
+        </div>
+      ) : (
+        <form onSubmit={handleFormSubmit}>
+          <div className="mb-4">
+            <input
+              ref={emailRef}
+              type="email"
+              required
+              className="rounded-md border focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary w-full"
+              placeholder="Email"
             />
-          </form>
-        )}
-      </div>
-    </AnonymousLayout>
+          </div>
+          <Button
+            as="button"
+            text="Verify Email"
+            type="CONFIRM"
+            extraStyles={"w-full"}
+          />
+        </form>
+      )}
+    </div>
   );
 };
 
