be6ffd29dc2fdc4870fb6b220fcd91c39a61c49c
Utkarsh Nawalgaria <utkarsh.n@olivecloud.in>
2022-11-06 19:01:15 +0530

    Minor fixes
    1. Add a 404 page.
    2. Remove the Settings option from the sidebar

--- web/src/components/layouts/authenticated.tsx
+++ web/src/components/layouts/authenticated.tsx
@@ -1,25 +1,23 @@
 import Sidebar from "../sidebar";
-import UserSettings from "../user-settings";
 import useUser, { TUserContext } from "../../context/UserProvider";
 
 const AuthenticatedLayout = ({ children }: { children: JSX.Element }) => {
   const { user, initials } = useUser() as TUserContext;
 
   return (
-    <div className="dark grid grid-rows-[60px_1fr] w-full h-screen overflow-hidden">
-      <header className="logo p-3 shadow-md flex justify-between items-center">
+    <div className="grid grid-rows-[60px_1fr] w-full h-screen overflow-hidden bg-gray-50">
+      <header className="logo p-3 shadow-md flex justify-between items-center bg-white">
         <div className="font-extrabold text-3xl">Tasks</div>
         <div className="mr-2 flex gap-2 items-center px-2 py-1 cursor-pointer rounded-md border">
           <span className="bg-red-100 p-2 rounded-full">{initials}</span>
           <span>{user?.name}</span>
         </div>
       </header>
-      <div className="grid grid-cols-[260px_1fr] bg-gray-100">
-        <aside className="w-[260px] shadow-xl bg-white my-4 rounded-md flex flex-col justify-between">
+      <div className="grid grid-cols-[260px_1fr]">
+        <aside className="w-[260px] m-4 flex flex-col justify-between">
           <Sidebar />
-          <UserSettings />
         </aside>
-        <main className="h-full p-4">
+        <main className="h-full p-6">
           <div className="h-full bg-white rounded-md shadow-md p-4">
             {children}
           </div>
