17f0ad632bc730e96027115ad3d7d8494d6559a8
Utkarsh Nawalgaria <utkarsh.n@olivecloud.in>
2022-11-20 18:11:47 +0530

    Removed the FastAPI backend. Will be replaced by django backend

--- server/apps/organization/schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import EmailStr
-from sqlmodel import SQLModel
-
-
-class OrganizationMember(SQLModel):
-    name: str = ""
-    email: EmailStr
-    invitation_status: str = "PENDING"
-    role: str = "MEMBER"
