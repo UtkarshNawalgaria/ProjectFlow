c37d8ef67968ccc0ebc4416b6539b641d53db7a6
Utkarsh Nawalgaria <utkarsh.n@olivecloud.in>
2022-12-04 17:57:00 +0530

    Integrate application with AWS
    In production, all the static assets collected from the `collectstatic`
    command will now be uploaded to S3.
    Also, all user uploaded media files will also be stored and served
    directly from S3

--- server/config/settings/base.py
+++ server/config/settings/base.py
@@ -34,6 +34,7 @@ THIRD_PARTY_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     "corsheaders",
+    'storages',
 ]
 
 CUSTOM_APPS = [
@@ -119,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- server/config/settings/production.py
+++ server/config/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["utkarshnawalgaria.com"]
+ALLOWED_HOSTS = ["tasks.utkarshnawalgaria.com"]
 
 DATABASES["default"] = {
     "ENGINE": "django.db.backends.postgresql",
@@ -17,6 +17,24 @@ DATABASES["default"] = {
 DEFAULT_FROM_EMAIL = "mail@utkarshnawalgaria.com"
 
 CORS_ALLOWED_ORIGINS = ["https://tasks.utkarshnawalgaria.com"]
+
+# AWS Settings
+AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+
+# STATIC
+AWS_STATIC_LOCATION = 'static'
+STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_STATIC_LOCATION}/"
+STATICFILES_STORAGE = 'libs.storage_backends.StaticStorage'
+
+# Media
+AWS_MEDIA_LOCATION = 'media'
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_MEDIA_LOCATION}/"
+DEFAULT_FILE_STORAGE = "libs.storage_backends.MediaStorage"
+
 
 # Production checks
 SECURE_SSL_REDIRECT = True
