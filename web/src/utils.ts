8e4f35b245b9f1dad9a4e061d848c1bf18b3637f
Utkarsh Nawalgaria <utkarsh.n@olivecloud.in>
2023-04-16 22:50:42 +0530

    added utility function to capitalize a string

--- web/src/utils.ts
+++ web/src/utils.ts
@@ -10,3 +10,16 @@ export function parseValidationErrorResponse(detail: ProcessedFormErrorType) {
 
   return formErrors;
 }
+
+export function capitalize(value: string) {
+  if (value.length == 0) return "";
+  if (value.length == 1) return value.toUpperCase();
+
+  let output = "";
+
+  for (const split_value of value.split(" ")) {
+    output += split_value.charAt(0).toUpperCase() + split_value.slice(1);
+  }
+
+  return output;
+}
