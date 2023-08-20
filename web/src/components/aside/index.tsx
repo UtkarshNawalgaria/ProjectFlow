4d51470c96703bbdd95297a19e66357d9a2b8367
Utkarsh Nawalgaria <utkarsh.n@olivecloud.in>
2023-08-20 16:33:20 +0530

    New Component: Aside
    This component is a sliding sidebar. It is hidden on the right
    side of the screen by default. It slides in from the right when
    user wants to open the sidebar.
    
    Wrapper components can be created around `Aside` to add
    data and functionality in the sidebar.

---
+++ web/src/components/aside/index.tsx
@@ -0,0 +1,34 @@
+import { HiX } from "react-icons/hi";
+
+export type AsideProps = {
+  open: boolean;
+  close: () => void;
+  children: JSX.Element[] | JSX.Element;
+  title?: string | JSX.Element;
+};
+
+const Aside = ({ children, open, close, title }: AsideProps) => {
+  return (
+    <aside
+      className={`fixed top-0 bottom-0 w-[46%] right-0 bg-slate-800 shadow-2xl p-4 transform-translate duration-500 ${
+        open ? "translate-x-0" : "translate-x-full"
+      }`}>
+      <header className="flex justify-between items-center mb-10">
+        {title ? (
+          <div className="text-xl font-semibold dark:text-grey-lightest">
+            {title}
+          </div>
+        ) : null}
+        <span className="mt-1 cursor-pointer">
+          <HiX
+            className="text-gray-500 dark:text-grey-lightest w-4 h-4"
+            onClick={close}
+          />
+        </span>
+      </header>
+      <div>{children}</div>
+    </aside>
+  );
+};
+
+export default Aside;
